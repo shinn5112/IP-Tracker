ee7ecef55f3c3cdce8e051cc66decd087cc694bc
shinn16 <shinn16@marshall.edu>
2016-03-27 17:26:00 -0400

    Smart update finished.

--- update.sh
+++ update.sh
@@ -4,18 +4,17 @@
 # Version 2.0
 # Last updated 3/27/16
 echo "Updating WAN_Checker.py and Setup.py to the newest version: pulling from GitHub"
-git pull > tmp
+git pull > tmp 2> tmp.error
 update=$( tail tmp | grep "Already up-to-date.")
 if [[ $update == "Already up-to-date." ]]
 then echo $update; rm tmp; exit
 fi
-update=$(tail tmp | grep "Could not read from remote repository.")
-if [[ $update == "Could not read from remote repository." ]]
+update=$(tail tmp.error | grep "fatal: Could not read from remote repository.")
+if [[ $update == "fatal: Could not read from remote repository." ]]
 then echo "Could not reach the repository, please check your internet connection and try again."; rm tmp; exit
-
 else echo "Update found, updating to newest version."
 fi
-rm tmp
+rm tmp tmp.error
 sudo -S cp -vf Setup.py WAN_Checker.py /opt/wan
 echo "Updating WAN_Checker.py and Setup.py to the newest version."
 cd /opt/wan
