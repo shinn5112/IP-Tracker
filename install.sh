9bbab8b1182eb3227bf8770c47dd3eddb88ce71d
shinn16 <shinn16@marshall.edu>
2016-04-08 21:48:14 -0400

    Optimizing install.sh

--- install.sh
+++ install.sh
@@ -6,23 +6,25 @@
 
 # saves github repo clone directory for crontab installation for auto-update.
 localRepo=$(pwd)
+sudo echo $localRepo > pwd.txt
 echo "Thank you for installing WAN-Tracker. Your install will begin soon."
 echo "Please ensure that you have Python 3 installed before executing this script."
 
-# makes a wan directory in the optional software folder, then places a copy of setup and WAN_Checker in it.
+# makes a wan directory in the optional software folder, then places a copy of setup, pwd.txt, and WAN_Checker in it.
 sudo -S mkdir -v /opt/wan
 echo "Moving files to /opt/wan folder."
-sudo cp -v Setup.py WAN_Checker.py auto-update.sh /opt/wan
+sudo cp -v Setup.py WAN_Checker.py pwd.txt /opt/wan
 cd /opt/wan
 # Changes to the new wan directory, and secures the program files
-sudo chmod +x Setup.py WAN_Checker.py auto-update.sh
-sudo chmod 700 Setup.py WAN_Checker.py auto-update.sh
+sudo chown root:root Setup.py WAN_Checker.py pwd.txt
+sudo chmod 544 pwd.txt
+sudo chmod +x Setup.py WAN_Checker.py
+sudo chmod 700 Setup.py WAN_Checker.py
 sleep 3
 echo "Starting setup program, please use Easy setup if you are a first time user."
 sleep 4
 # clears the screen, then runs the setup.py script
 clear
-sudo chown root:root Setup.py WAN_Checker.py
 sudo ./Setup.py
 clear
 # clears the screen after exiting setup and then secures the generated files.  The installs automation crontab.
@@ -42,11 +44,6 @@ read -p "Would you like to receive automatic updates? y/N: " answer
 if [[ $answer == "y" ]]
 then echo "Installing auto-update crontab."; (sudo -S crontab -u root -l; echo -n "0 0 * * * "; echo -n $localRepo; echo "/auto-update.sh 2>> /opt/wan/updateErrorLog.txt") | sudo crontab -u root -; sudo touch updateErrorLog.txt updateLog.txt; sudo chown root:root updateErrorLog.txt updateLog.txt; sudo chmod 744 updateErrorLog.txt updateLog.txt; sudo chmod -x updateErrorLog.txt updateLog.txt; echo "Auto updates enabled."
 fi
-
-# Records git repo for auto-updater
-sudo echo $localRepo > pwd.txt
-sudo chown root:root pwd.txt
-sudo chmod 544 pwd.txt
 
 # Ends installation, prints a messages to the user letting them know.
 echo "Install complete. Please check your designated recipient email in two minutes or so."
